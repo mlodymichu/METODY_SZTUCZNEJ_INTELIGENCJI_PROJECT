ee2a77fcc3eced13184cf12217a58049cc9f8754
mlodymichu <Igormiczynski1@gmail.com>
2025-04-03 22:46:07 +0200

    MSI zrobiony wykres PLEC->MARKA TELEFONU, zaczete nowe wykresy

--- projekt_data_exploration.py
+++ projekt_data_exploration.py
@@ -12,6 +12,9 @@ data_set = pd.read_csv("mobile_phone_data_set.csv", delimiter=";")
 #print(data_set.dtypes)
 #print(data_set.describe(include=[np.number]))
 
+print(data_set.iloc[:,1:].describe(include=[np.number]))
+
+
 
 data_set.loc[data_set['Device Model'] == 'iPhone 12', "Release date"] = datetime(2020,10,13)
 data_set.loc[data_set['Device Model'] == 'Xiaomi Mi 11', "Release date"] = datetime(2022,2,22)
@@ -33,15 +36,30 @@ np.savetxt("mobile_phone_data_set_added_data.csv", data_set,delimiter=",", fmt="
 
 #print(data_set.groupby('Device Model')['Gender'].count())
 
-to_plot_gender_device_model = data_set.groupby('Gender')['Device Model'].value_counts()
-print(to_plot_gender_device_model)
+each_model = set()
+for x in data_set['Device Model']:
+    each_model.add(x)
+print(each_model)
+indx = np.arange(len(each_model))
+print(indx)
 
-fig, axs = plt.subplots(1,2)
-axs[1]= data_set[data_set["Gender"]=='Female'].groupby('Gender')['Device Model'].value_counts().plot.bar()
-axs[0]= data_set[data_set["Gender"]=='Male'].groupby('Gender')['Device Model'].value_counts().plot.bar()
+# to_plot_gender_device_model = data_set.groupby('Gender')['Device Model'].value_counts()
+# print(to_plot_gender_device_model)
+
+
+#TO JEST WYKRES MARKI->PLEC
+bar_width = 0.35
+
+fig1, ax = plt.subplots(figsize=(10,12))
+barFemale = ax.bar(indx - bar_width/2, data_set[data_set["Gender"]=='Female'].groupby('Gender')['Device Model'].value_counts(), bar_width, label='Female')
+barMale = ax.bar(indx + bar_width/2, data_set[data_set["Gender"]=='Male'].groupby('Gender')['Device Model'].value_counts(), bar_width, label='Male')
+ax.set_xticks(indx)
+ax.set_xticklabels(each_model,rotation=45)
+ax.legend()
 plt.show()
 
-# check_size= data_set[data_set["Gender"]=='Female'].groupby('Gender')['Device Model'].value_counts()
-# fig, axs = plt.subplots(1,2)
-# axs[0] = plt.bar(data_set[data_set["Gender"]=='Female'].groupby('Gender')['Device Model'].value_counts(),range(check_size.size))
-# plt.show()
+color_dict = {'iPhone 12':'red', 'Xiaomi Mi 11':'blue', 'Google Pixel 5':'black', 'Samsung Galaxy S21':'green','OnePlus 9':'purple'}
+
+fig2, ax = plt.subplots()
+ax.scatter(data_set['App Usage Time (min/day)'],data_set['User ID'], c=[color_dict[i] for i in data_set['Device Model']])
+plt.show()
